caa0937ae5e64036f929e0b97800fe4601e0760f
akbar <akbar.zulfikar@batumbu.id>
2023-11-03 14:32:35 +0700

    Add Card tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <div className="w-full h-screen text-4xl text-blue-600 flex items-center justify-center">
+      <div className="max-w-sm mx-auto overflow-hidden rounded shadow-lg">
+        <img
+          className="w-full h-auto"
+          src="https://via.placeholder.com/300"
+          alt="Placeholder"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Card Title</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            Category 1
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+            Category 2
+          </span>
+        </div>
+      </div>
+      <div className="w-full h-screen text-4xl text-blue-600 flex items-center justify-center">
       Hello world, Try vite and tailwind
      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
