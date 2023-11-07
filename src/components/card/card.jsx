ab5c12783940903b0666e3918faf94f0d922f8ea
akbar <akbar.zulfikar@batumbu.id>
2023-11-07 14:40:04 +0700

    setup folder

---
+++ src/components/card/card.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div className="max-w-sm mx-auto overflow-hidden rounded shadow-lg">
+      <img
+        className="w-full h-auto"
+        src="https://via.placeholder.com/300"
+        alt="Placeholder"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Card Title</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          Category 1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+          Category 2
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default card;
