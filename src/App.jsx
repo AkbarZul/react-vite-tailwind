84c6046e8d5028d8a7a058c37d573c451d7c2289
akbar <akbar.zulfikar@batumbu.id>
2023-11-15 16:04:53 +0700

    set, get and remove cookie

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import Cookies from "js-cookie";
+
 function App() {
+  const setCookie = () => {
+    Cookies.set("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9", {
+      expires: 7,
+    });
+  };
+
+  const getCookie = () => {
+    alert(Cookies.get("token"));
+  };
+
+  const removeCookie = () => {
+    Cookies.remove("token");
+  };
   return (
     <>
-      <div className="max-w-sm mx-auto overflow-hidden rounded shadow-lg">
+      {/* <div className="w-full h-screen text-4xl text-blue-600 flex items-center justify-center">
+        Hello world, Try vite and tailwind
+      </div> */}
+
+      <div className="max-w-sm mx-auto overflow-hidden rounded shadow-lg mt-10">
         <img
           className="w-full h-auto"
           src="https://via.placeholder.com/300"
@@ -22,9 +41,53 @@ function App() {
             Category 2
           </span>
         </div>
-      </div>
-      <div className="w-full h-screen text-4xl text-blue-600 flex items-center justify-center">
-        Hello world, Try vite and tailwind
+        <div className="flex justify-around mb-10 mt-10">
+          <div
+            className="relative inline-block text-lg group cursor-pointer"
+            onClick={setCookie}
+          >
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span className="relative">Set Cookies</span>
+            </span>
+            <span
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </div>
+
+          <div
+            className="relative inline-block text-lg group cursor-pointer"
+            onClick={getCookie}
+          >
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span className="relative">Get Cookies</span>
+            </span>
+            <span
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </div>
+        </div>
+        <div className="flex justify-center mb-10 mt-10">
+          <div
+            className="relative inline-block text-lg group cursor-pointer"
+            onClick={removeCookie}
+          >
+            <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+              <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+              <span className="relative">Remove Cookies</span>
+            </span>
+            <span
+              className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+              data-rounded="rounded-lg"
+            ></span>
+          </div>
+        </div>
       </div>
     </>
   );
